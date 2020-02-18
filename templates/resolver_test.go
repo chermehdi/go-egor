5f5d2be0de640d24297808ba688889359a7e7d53
Mehdi Cheracher <mehdi.cheracher@gmail.com>
2020-02-18 22:59:14 +0100

    add resolver tests back

--- templates/resolver_test.go
+++ templates/resolver_test.go
@@ -1,25 +1,31 @@
 package templates
 
-// func TestResolveTemplateByLanguageJava(t *testing.T) {
-// 	temp, err := ResolveTemplateByLanguage("java")
-// 	assert.NoError(t, err)
-// 	assert.Equal(t, JavaTemplate, temp)
-// }
-//
-// func TestResolveTemplateByLanguagePython(t *testing.T) {
-// 	temp, err := ResolveTemplateByLanguage("python")
-// 	assert.NoError(t, err)
-// 	assert.Equal(t, PythonTemplate, temp)
-// }
-//
-// func TestResolveTemplateByLanguageCpp(t *testing.T) {
-// 	temp, err := ResolveTemplateByLanguage("cpp")
-// 	assert.NoError(t, err)
-// 	assert.Equal(t, CppTemplate, temp)
-// }
-//
-// func TestResolveTemplateByLanguageUnknow(t *testing.T) {
-// 	temp, err := ResolveTemplateByLanguage("dummy")
-// 	assert.Error(t, err)
-// 	assert.Empty(t, temp)
-// }
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestResolveTemplateByLanguageJava(t *testing.T) {
+	temp, err := ResolveTemplateByLanguage("java")
+	assert.NoError(t, err)
+	assert.Equal(t, JavaTemplate, temp)
+}
+
+func TestResolveTemplateByLanguagePython(t *testing.T) {
+	temp, err := ResolveTemplateByLanguage("python")
+	assert.NoError(t, err)
+	assert.Equal(t, PythonTemplate, temp)
+}
+
+func TestResolveTemplateByLanguageCpp(t *testing.T) {
+	temp, err := ResolveTemplateByLanguage("cpp")
+	assert.NoError(t, err)
+	assert.Equal(t, CppTemplate, temp)
+}
+
+func TestResolveTemplateByLanguageUnknow(t *testing.T) {
+	temp, err := ResolveTemplateByLanguage("dummy")
+	assert.Error(t, err)
+	assert.Empty(t, temp)
+}
