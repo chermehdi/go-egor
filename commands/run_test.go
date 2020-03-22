4221393f040f71fb2693eec1fd3860a3ed82f4df
Abdelaziz Eroui <kapi.abdel@gmail.com>
2020-03-22 19:48:16 +0000

    Add timed exeuction test

--- commands/run_test.go
+++ commands/run_test.go
@@ -1,7 +1,21 @@
 package commands
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"os/exec"
+	"testing"
+)
 
-func TestNothing(t *testing.T) {
-	// NO-OP
+func TestTimedOutExecution(t *testing.T) {
+	cmd := exec.Command("sleep", "1")
+	status, err := timedExecution(cmd, 999.99)
+	assert.Equal(t, err, nil)
+	assert.Equal(t, status, TO)
+}
+
+func TestTimedExecution(t *testing.T) {
+	cmd := exec.Command("sleep", "1")
+	status, err := timedExecution(cmd, 1250)
+	assert.Equal(t, err, nil)
+	assert.Equal(t, status, OK)
 }
