8b958b6a712303d83556ef6fb8b62e437e741a1d
chermehdi <mehdi.cheracher@gmail.com>
2021-01-09 21:16:59 +0000

    Fix failing build

--- commands/run_test.go
+++ commands/run_test.go
@@ -1,24 +1,25 @@
 package commands
 
 import (
+	"github.com/chermehdi/egor/config"
+	"github.com/chermehdi/egor/utils"
+	"github.com/stretchr/testify/assert"
 	"os/exec"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestTimedOutExecution(t *testing.T) {
 	cmd := exec.Command("sleep", "1.1")
-	status, _, err := timedExecution(cmd, 1000+TimeOutDelta)
+	status, _, err := utils.ExecuteWithTimeout(cmd, 1000+config.TimeOutDelta)
 	assert.Equal(t, err, nil)
-	assert.Equal(t, status, TO)
+	assert.Equal(t, status, config.TO)
 }
 
 func TestTimedExecution(t *testing.T) {
 	cmd := exec.Command("sleep", "1")
-	status, _, err := timedExecution(cmd, 1000+TimeOutDelta)
+	status, _, err := utils.ExecuteWithTimeout(cmd, 1000+config.TimeOutDelta)
 	assert.Equal(t, err, nil)
-	assert.Equal(t, status, OK)
+	assert.Equal(t, status, config.OK)
 }
 
 func TestChecker_TokenBasedCheckerShouldIgnoreSpaces(t *testing.T) {
